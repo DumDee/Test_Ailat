e2bd6ff5220f474c10db3c60263ac22da2dc7ea5
DumDee <argentenium@gmail.com>
2025-05-19 18:11:04 +0500

    Изменения в модели stocks

--- stocks/serializers.py
+++ stocks/serializers.py
@@ -32,3 +32,33 @@ class StockSerializer(serializers.ModelSerializer):
             'trading_view_ticker', 'tinkoff_link',
             'last_updated',
         ]
+
+class StockDetailSerializer(serializers.ModelSerializer):
+    is_compliant = serializers.SerializerMethodField()
+    icon_url = serializers.SerializerMethodField()
+    screening = serializers.SerializerMethodField()
+    status_history = ComplianceStatusHistorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Stock
+        fields = [
+            'id', 'name', 'ticker', 'exchange', 'country', 'sector', 'industry',
+            'current_price', 'change_value', 'change_percent', 'currency',
+            'compliance_status', 'is_compliant', 'icon_url', 'last_updated',
+            'status_history', 'screening'
+        ]
+
+    def get_is_compliant(self, obj):
+        return obj.compliance_status == 'compliant'
+
+    def get_icon_url(self, obj):
+        request = self.context.get('request')
+        if obj.icon_url:
+            return request.build_absolute_uri(obj.icon_url)
+        return None
+
+    def get_screening(self, obj):
+        screening = ShariahScreening.objects.filter(stock=obj).order_by('-screening_date').first()
+        if screening:
+            return ShariahScreeningSerializer(screening).data
+        return None
--- stocks/views.py
+++ stocks/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from .models import Stock
-from .serializers import StockSerializer
+from .serializers import StockSerializer, StockDetailSerializer
 
 
 class StockViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Stock.objects.all()
-    serializer_class = StockSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return StockDetailSerializer
+        return StockSerializer
